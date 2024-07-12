f44e991be4d5a81efbaf1748ffadaf98c21550b3
Graham Boyle <olayile1999@gmail.com>
2024-07-12 17:15:50 +0100

    updated

---
+++ data-structure/des-obj.js
@@ -0,0 +1,44 @@
+'use strict';
+
+import books from './main.js';
+
+// solution 1
+const { title, author, ISBN } = books[0];
+console.log(title, author, ISBN);
+
+// solution 2
+const { keywords: tags } = books[0];
+console.log(tags);
+
+// solution 3
+const { language, programmingLanguage = 'unknown' } = books[6];
+console.log(language, programmingLanguage);
+
+// solution 4
+let bookTitle = 'unknown';
+let bookAuthor = 'unknown';
+
+({ title: bookTitle, author: bookAuthor } = books[0]);
+console.log(bookAuthor, bookTitle);
+
+// solution 5
+const {
+  thirdParty: {
+    goodreads: { rating: bookRating },
+  },
+} = books[0];
+console.log(bookRating);
+
+//solution 6
+function printBookInfo({ title, author, publicationDate = 'Unknown' }) {
+  console.log(`${title} by ${author},year ${publicationDate.slice(0, 4)}`);
+}
+
+printBookInfo(books[0]);
+printBookInfo(books[1]);
+printBookInfo(books[2]);
+printBookInfo(books[3]);
+printBookInfo(books[4]);
+printBookInfo(books[5]);
+printBookInfo(books[6]);
+printBookInfo(books[7]);
